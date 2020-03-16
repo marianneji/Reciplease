c708ea08a9c0abc3849f2b838bb641e2b35254d2
marianneji <marianneji@gmail.com>
2020-03-16 15:55:02 +0100

    add cellID for the main scene

---
+++ Reciplease/SupportingFiles/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Reciplease
+//
+//  Created by Graphic Influence on 13/03/2020.
+//  Copyright © 2020 marianne massé. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let collectionViewCellID = "recipeCell"
+    static let tableViewCellID = "FavoriteRecipeCell"
+    static let ingredientCellId = "Ingredients"
+}
