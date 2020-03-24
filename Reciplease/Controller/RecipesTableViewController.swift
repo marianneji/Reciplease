7d1b27ee41dfeb9f18b9b26b41e7baff254e4a86
marianneji <marianneji@gmail.com>
2020-03-24 17:11:16 +0100

    clean code

--- Reciplease/Controller/RecipesTableViewController.swift
+++ Reciplease/Controller/RecipesTableViewController.swift
@@ -17,24 +17,23 @@ class RecipesTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        recipesTableView.reloadData()
         let nibName = UINib(nibName: "RecipeTableViewCell", bundle: nil)
         recipesTableView.register(nibName, forCellReuseIdentifier: "recipeCell")
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        print("\(String(describing: recipeData?.hits.count)) dans le recipeTableViewController ")
-        return recipeData?.hits.count ?? 0
+        guard let totalRecipes = recipeData?.hits.count else {
+            didFailWithError(message: RecipeSearchManager.messageError)
+            return 0
+        }
+        return totalRecipes
     }
-
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "recipeCell", for: indexPath) as? RecipeTableViewCell else {
@@ -47,16 +46,6 @@ class RecipesTableViewController: UITableViewController {
         return cell
     }
 
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        let translation = CATransform3DTranslate(CATransform3DIdentity, 0, 50, 0)
-        cell.layer.transform = translation
-        cell.alpha = 0
-        UIView.animate(withDuration: 0.75) {
-            cell.layer.transform = CATransform3DIdentity
-            cell.alpha = 1
-        }
-    }
-
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 300
     }
@@ -67,22 +56,21 @@ class RecipesTableViewController: UITableViewController {
 
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
         return true
     }
-
-
-
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
         guard segue.identifier == "showDetailRecipe" else { return }
         guard let detailVC = segue.destination as? DetailRecipeViewController else { return }
         guard let indexPath = tableView.indexPathForSelectedRow else { return }
         detailVC.selectedRecipe = recipeData?.hits[indexPath.row]
         detailVC.vcOne = true
-    }    
+    }
+
+    @IBAction func addMoreRecipesPressed(_ sender: UIBarButtonItem) {
+        
+        
+    }
 }
